8e8df9e4cd62f3d517eb54619092f55cd1e753d2
Ndungutse Charles <95475354+NdungutseCharles103@users.noreply.github.com>
2023-11-07 15:49:24 +0200

    feat: user role + avatar

--- src/components/dashboard/crud/ViewUser.tsx
+++ src/components/dashboard/crud/ViewUser.tsx
@@ -10,14 +10,15 @@ interface Props {
 const ViewUser: FC<Props> = ({ user }) => {
   return (
     <div className="flex flex-col p-3 items-center w-full">
-      <div className="flex flex-col gap-y-2">
+      <div className="flex flex-col gap-y-2 items-center">
         <Avatar
           size={"xl"}
-          src={"https://avatars.githubusercontent.com/u/55942632?v=4"}
+          src={`https://ui-avatars.com/api/?name=${user?.username}+${user?.email}&bold=true`}
         />
-        <div className="flex w-full flex-col">
+        <div className="flex items-center w-full flex-col">
           <p className="text-lg font-semibold ">{user?.username}</p>
           <p className=" opacity-80">{user?.department?.name}</p>
+          <p className=" opacity-80">{user?.roles[0].roleName}</p>
         </div>
       </div>
       <Divider w={"100%"} my={"md"} />
